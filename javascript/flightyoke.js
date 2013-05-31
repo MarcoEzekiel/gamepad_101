1b74bd3c369ee7a14125df73cbadd02b4a16ffd9
Mark A Harris <T_walnut>
2013-05-31 17:40:52 -0600

    more guages for flight yoke controller

--- javascript/flightyoke.js
+++ javascript/flightyoke.js
@@ -3,6 +3,10 @@ window.onload = function() {
 	var mesh = [];
 	function de2ra(degree)   { return degree*(Math.PI/180); }
 
+	var circPoints =[];
+	var indicators = [];
+	var text = [];
+
 	function showPadDetails(){
 
 		var gamepadSupportAvailable = Modernizr.gamepads;
@@ -120,7 +124,7 @@ window.onload = function() {
 				var points = shape.createPointsGeometry();
 				
 
-				shape_line[index] = new THREE.Line( points, new THREE.LineBasicMaterial( { color: color, linewidth: 4 } ) );
+				shape_line[index] = new THREE.Line( points, new THREE.LineBasicMaterial( { color: color, linewidth: 2 } ) );
 				shape_line[index].position.set( x, y, z );
 				shape_line[index].rotation.set( rx, ry, rz );
 				shape_line[index].scale.set( s, s, s );
@@ -144,44 +148,116 @@ window.onload = function() {
 
 			}
 
-			function addPlanet(xDistance, y, z, radius, segWidth, segHeight, color1, color2, index , motionAngle, sunIndex, sun){
-				var planetDarkMaterial = new THREE.MeshBasicMaterial( { color: color1 } );
-				var planetWireframeMaterial = new THREE.MeshBasicMaterial( { color: color2, wireframe: true, transparent: true } ); 
-				var planetMaterial = [ planetDarkMaterial, planetWireframeMaterial ]; 
 
-				planet[index] = THREE.SceneUtils.createMultiMaterialObject( 
-				new THREE.SphereGeometry( radius, segWidth, segHeight  ), 
-				planetMaterial );
-				planet[index].radius = radius;
+			function drawGuage(x,y,z,radius,index,color,axisController){
 
-				sun == 0 ? pPos = sol[sunIndex].radius: pPos = planet[sun].radius
-				planet[index].position.set( pPos + xDistance ,y,z);
-				planet[index].motionAngle = motionAngle;
-				scene.add( planet[index] );
-			}
+				circPoints = [];
+				function setCirclePointCoordinates(radius){
+					for(var iDeg= 1; iDeg <=360 ; iDeg++){
+						var xN = radius * Math.cos(2 * Math.PI * iDeg / 360).toFixed(6);
+						var yN = radius * Math.sin(2 * Math.PI * iDeg / 360).toFixed(6); 
+						circPoints.push(new THREE.Vector2 (xN+x,yN+y));
+					}
+				}
+				setCirclePointCoordinates(radius);
 
-
-			function addSun(x, y, z, radius, segWidth, segHeight, index ){
-				// var darkMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-				// var wireframeMaterial = new THREE.MeshBasicMaterial( { color: 0xFF6103, wireframe: true, transparent: true } ); 
-				// var multiMaterial = [ darkMaterial, wireframeMaterial ]; 
 				
-				index % 2 == 0 ? img = "images/fire.jpeg": img = "images/blue.jpeg"
-				var fire = new THREE.MeshBasicMaterial({
-				    map: THREE.ImageUtils.loadTexture(img)
-				});
+				var circShape = new THREE.Shape( circPoints );
+				var extrudeSettings = { amount: 20 }; // bevelSegments: 2, steps: 2 , bevelSegments: 5, bevelSize: 8, bevelThickness:5;
+				addSolidLineShape( circShape, extrudeSettings, color, 0, 0, 0, 0, 0, 0, 1, index );
+				//addExtruded3DShape( circShape, extrudeSettings, Math.random() * 0xffffff, 0, 0, 0, 0, 0,0, .8, index );
 
-				var sphereGeometry = new THREE.SphereGeometry(radius, segWidth, segHeight);
-				var sphereTexture = new THREE.ImageUtils.loadTexture( img );
-				var sphereMaterial = new THREE.MeshBasicMaterial( { map: sphereTexture } );
-				sol[index] = new THREE.Mesh(sphereGeometry.clone() , sphereMaterial);
+				var lineGeometry = new THREE.Geometry();
+				var vertArray = lineGeometry.vertices;
+				vertArray.push( new THREE.Vector3(x, y ,z), new THREE.Vector3(x,radius,z));
+				lineGeometry.computeLineDistances();
+				var lineMaterial = new THREE.LineBasicMaterial( { color:color } );
 
-				sol[index].position.set(x, y, z);
-				sol[index].radius = radius;
-				scene.add( sol[index] );
+
+			 	indicators[index] = new THREE.Line( lineGeometry, lineMaterial );
+			 	indicators[index].startX = x;
+			 	indicators[index].startY = y;
+				indicators[index].startZ = z;
+				indicators[index].radius = radius;
+				indicators[index].color = color;
+
+				scene.add(indicators[index]);
+
+				// create a canvas element
+				function addText(text, x,y,z){
+					var canvas1 = document.createElement('canvas');
+					var context1 = canvas1.getContext('2d');
+					context1.font = "Bold 20px Arial";
+					c = "#"+Math.round(color).toString(16);
+					context1.fillStyle = c;
+				    context1.fillText(text, 0, 50);
+				    
+					// canvas contents will be used for a texture
+					var texture1 = new THREE.Texture(canvas1) 
+					texture1.needsUpdate = true;
+				      
+				    var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
+				    material1.transparent = true;
+
+				    var mesht = new THREE.Mesh(
+				        new THREE.PlaneGeometry(canvas1.width, canvas1.height),
+				        material1
+				      );
+					mesht.position.set(x,y,z);
+					scene.add( mesht );
+				}
+				var rInner = radius-20;
+				for(var tp = 0; tp <=11; tp++){
+
+						xT = rInner * Math.cos(2 * Math.PI * (tp +135) / 12).toFixed(6)+140;
+						yT = rInner * Math.sin(2 * Math.PI * (tp +135) / 12).toFixed(6)-28; 
+						//alert(x+135+' : '+y+165);
+						addText(invertDisplay(tp), xT+x, yT+y ,z , tp);
+				}
+
 			}
 
+			function invertDisplay(n){
+				switch(n){
+					case 0:
+						return '';
+						break;
+					case 1:
+						return 110;
+						break;
+					case 2:
+						return 100;
+						break;
+					case 3:
+						return 90;
+						break;
+					case 4:
+						return 80;
+						break;
+					case 5:
+						return 70;
+						break;
+					case 6:
+						return 60;
+						break;
+					case 7:
+						return 50;
+						break;
+					case 8:
+						return 40;
+						break;
+					case 9:
+						return 30;
+						break;
+					case 10:
+						return 20;
+						break;
+					case 11:
+						return 10;
+						break;
 
+				}
+			}
 			// SKYBOX/FOG
 			var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
 			var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
@@ -194,101 +270,94 @@ window.onload = function() {
 			// CUSTOM //
 			////////////
 
+			drawGuage(0,0,0,200,1,Math.random() * 0xffffff,null);
+			drawGuage(-400,0,0,160,2,Math.random() * 0xffffff,null);
+			drawGuage(400,0,0,160,3,Math.random() * 0xffffff,null);
+			//drawGuage(0,-400,0,120,4,Math.random() * 0xffffff,null);
 
-			// for(var iDeg= 1; iDeg <=360 ; iDeg++){
-			// 	x = 200 * Math.cos(2 * Math.PI * iDeg / 360).toFixed(6);
-			// 	y = 200 * Math.sin(2 * Math.PI * iDeg / 360).toFixed(6) +200; 
-
-			// 	xTo = 200 * Math.cos(2 * Math.PI * (iDeg+1) / 360).toFixed(6);
-			// 	yTo = 200 * Math.sin(2 * Math.PI * (iDeg+1) / 360).toFixed(6) +200; 
-
-
-
-			// 	var lineGeometry = new THREE.Geometry();
-			// 	var vertArray = lineGeometry.vertices;
-			// 	vertArray.push( new THREE.Vector3(x, y, 0), new THREE.Vector3(xTo, yTo, 0));
-			// 	lineGeometry.computeLineDistances();
-			// 	var lineMaterial = new THREE.LineBasicMaterial( { color:0xff00000 } );
-			//  	line = new THREE.Line( lineGeometry, lineMaterial );
-			// 	scene.add(line);
-			// }
-
-			var circPoints =[]
-			function drawCircle(){
-				for(var iDeg= 1; iDeg <=360 ; iDeg++){
-					x = 200 * Math.cos(2 * Math.PI * iDeg / 360).toFixed(6);
-					y = 200 * Math.sin(2 * Math.PI * iDeg / 360).toFixed(6) +200; 
-
-					// xTo = 200 * Math.cos(2 * Math.PI * (iDeg+1) / 360).toFixed(6);
-					// yTo = 200 * Math.sin(2 * Math.PI * (iDeg+1) / 360).toFixed(6) +200; 
-					
-					circPoints.push(new THREE.Vector2 (x,y));
-				}
-			}
-
-			drawCircle();
-
-			var index=0;
-			var circShape = new THREE.Shape( circPoints );
-			var extrudeSettings = { amount: 20 }; // bevelSegments: 2, steps: 2 , bevelSegments: 5, bevelSize: 8, bevelThickness:5;
-			addSolidLineShape( circShape, extrudeSettings, 0xff0000, 0, 0, 0, 0, 0, 0, 1, index );
-			//addExtruded3DShape( circShape, extrudeSettings, Math.random() * 0xffffff, 0, 0, 0, 0, 0,0, .8, index );
-
-
-
-
-
-
-
-
-
-			var lineGeometry = new THREE.Geometry();
-			var vertArray = lineGeometry.vertices;
-			vertArray.push( new THREE.Vector3(0,200,0), new THREE.Vector3(0,400,0));
-			lineGeometry.computeLineDistances();
-			var lineMaterial = new THREE.LineBasicMaterial( { color:0xff00000 } );
-		 	indicator1 = new THREE.Line( lineGeometry, lineMaterial );
-			scene.add(indicator1);
-
+		
 
 		}
-
+		var cAxis;
 		function animate() 
 		{
+
+
+		var startX = [];
+		var startY = [];
+		var startZ = [];
+		var radius = [];
+		var color = [];
 			var gamepads = navigator.webkitGetGamepads();
 
 			var pad = gamepads[0];
 			if( pad )
 			{
 		
-			
-		
 				// drift protection as axis controllers do not return 0
-				var cxaxis0  = Math.round( pad.axes[0] * 10 ) / 10; // yoke pull/push
-				var cyaxis1  = Math.round( pad.axes[1] * 10 ) / 10; // yoke left/right
-				var czaxis2  = Math.round( pad.axes[2] * 10 ) / 10; // throttles left
-				var cxaxis3  = Math.round( pad.axes[3] * 10 ) / 10; // throttle right 
-				var cyaxis4  = Math.round( pad.axes[4] * 10 ) / 10; // throttle center
-				var czaxis5  = Math.round( pad.axes[5] * 10 ) / 10; 
-				var cxaxis6  = Math.round( pad.axes[6] * 10 ) / 10; 
-				var cyaxis7  = Math.round( pad.axes[7] * 10 ) / 10; 
-				var czaxis8  = Math.round( pad.axes[8] * 10 ) / 10; 
-				var czaxis9  = Math.round( pad.axes[9] * 10 ) / 10; // right thumb
+				var caxis0  = Math.round( pad.axes[0] * 10 ) / 10; // yoke pull/push
+				var caxis1  = Math.round( pad.axes[1] * 10 ) / 10; // yoke left/right
+				var caxis2  = Math.round( pad.axes[2] * 10 ) / 10; // throttles left
+				var caxis3  = Math.round( pad.axes[3] * 10 ) / 10; // throttle right 
+				var caxis4  = Math.round( pad.axes[4] * 10 ) / 10; // throttle center
+				var caxis5  = Math.round( pad.axes[5] * 10 ) / 10; 
+				var caxis6  = Math.round( pad.axes[6] * 10 ) / 10; 
+				var caxis7  = Math.round( pad.axes[7] * 10 ) / 10; 
+				var caxis8  = Math.round( pad.axes[8] * 10 ) / 10; 
+				var caxis9  = Math.round( pad.axes[9] * 10 ) / 10; // right thumb
 
 
-				var iDeg = (1 + czaxis2 ) *180 +90;
-				var x = 180 * Math.cos(2 * Math.PI * iDeg / 360).toFixed(6);
-				var y = 180 * Math.sin(2 * Math.PI * iDeg / 360).toFixed(6) +200; 
+				for( var gIdx = 1; gIdx <=3; gIdx++ ){
 
-				scene.remove( indicator1 );
-				var lineGeometry = new THREE.Geometry();
-				var vertArray = lineGeometry.vertices;
-				vertArray.push( new THREE.Vector3(0,200,0), new THREE.Vector3(x,y,0));
-				lineGeometry.computeLineDistances();
-				var lineMaterial = new THREE.LineBasicMaterial( { color: 0xff0000 } );
-			 	indicator1 = new THREE.Line( lineGeometry, lineMaterial );
-				scene.add(indicator1);
+					switch(gIdx){
+						case 1:
+						  cAxis  = caxis2;
+						  break;
+						case 2:
+						  cAxis  = caxis3;
+						  break;
+						case 3:
+						  cAxis  = caxis4;
+						  break;
+						case 4:
+						  cAxis  = caxis9;
+						  break;
+						default:
+						  alert('etf2');
+						  break;
+					}
 
+					var iDeg = (1 + cAxis ) * 180 + 90;
+
+					var x = indicators[gIdx].radius * Math.cos(2 * Math.PI * iDeg / 360).toFixed(6);
+					var y = indicators[gIdx].radius * Math.sin(2 * Math.PI * iDeg / 360).toFixed(6); 
+		
+
+					 startX[gIdx] = indicators[gIdx].startX;
+					 startY[gIdx] = indicators[gIdx].startY;
+					 startZ[gIdx] = indicators[gIdx].startZ;
+					 radius[gIdx] = indicators[gIdx].radius;
+					 color[gIdx] = indicators[gIdx].color;
+
+					scene.remove( indicators[gIdx] );
+
+					// creat replacement
+					var lineGeometry = new THREE.Geometry();
+					var vertArray = lineGeometry.vertices;
+
+					vertArray.push( new THREE.Vector3(startX[gIdx],startY[gIdx],startZ[gIdx]), new THREE.Vector3(x+startX[gIdx],y+startY[gIdx],0));
+
+					lineGeometry.computeLineDistances();
+					var lineMaterial = new THREE.LineBasicMaterial( { color: color[gIdx] } );
+				 	indicators[gIdx] = new THREE.Line( lineGeometry, lineMaterial );
+
+					indicators[gIdx].startX = startX[gIdx];
+					indicators[gIdx].startY = startY[gIdx];
+					indicators[gIdx].startZ = startZ[gIdx];
+					indicators[gIdx].radius = radius[gIdx];
+					indicators[gIdx].color  = color[gIdx];
+					scene.add(	indicators[gIdx]	);
+				}
 			
 			}
 			
